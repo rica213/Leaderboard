7ab72e2454d52ec24303223e2a684a4df3d5ddf7
rica213 <25f5k252l@mozmail.com>
2023-02-08 19:33:52 +0300

    Add function addScore

---
+++ src/modules/add-score.js
@@ -0,0 +1,24 @@
+const addScore = async (user, score) => {
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QwbJ6ANGodRCNyIW8VCA/scores/",
+    {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        "user": user,
+        "score": score
+          }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const result = await response.json();
+  return result;
+};
+
+export default addScore;
