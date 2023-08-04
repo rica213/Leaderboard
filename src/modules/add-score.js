830dc1dcc9c87ade5b65415e47b297856f2ae709
rica213 <25f5k252l@mozmail.com>
2023-08-04 21:44:32 +0300

    Fix linters

--- src/modules/add-score.js
+++ src/modules/add-score.js
@@ -1,16 +1,16 @@
 const addScore = async (user, score) => {
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qD7zBz3UYQ6xbdlDx7x9/scores/",
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qD7zBz3UYQ6xbdlDx7x9/scores/',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
       body: JSON.stringify({
         user,
         score,
       }),
-    }
+    },
   );
 
   if (!response.ok) {
--- src/modules/display-scores.js
+++ src/modules/display-scores.js
@@ -1,6 +1,5 @@
 const displayScores = async () => {
-  const requestURL =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qD7zBz3UYQ6xbdlDx7x9/scores/";
+  const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qD7zBz3UYQ6xbdlDx7x9/scores/';
   const request = new Request(requestURL);
 
   const response = await fetch(request);
